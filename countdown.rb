da892fb4a5d5251dc1f62c885d200716b1e4d000
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-13 21:16:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+def countdown(time)
+  time = 10
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+  if time < 12
+    "HAPPY NEW YEAR!"
+  end
+end
 
-def countdown
+def countdown_with_sleep(sleep)
+  sleep = 5
+  while sleep > 4
+    sleep += 1
+    puts "#{sleep}"
+  end
 end
